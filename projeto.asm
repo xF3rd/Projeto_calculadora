2546afcff31389b5c04b93feec928ee652f67131
xF3rd <fernando.bordin.correa@gmail.com>
2022-10-18 23:20:00 -0300

    comecei a fazer a multiplicacao

--- projeto.asm
+++ projeto.asm
@@ -32,7 +32,9 @@
         je SUBTRACAO
 
         cmp al,033h
-        ;je MULTIPLICACAO
+        je MULTIPLICACAO1
+        MULTIPLICACAO1:
+            jmp MULTIPLICACAO
 
         cmp al,034h
         ;je DIVICAO
@@ -152,8 +154,211 @@
 
 
         MULTIPLICACAO:
-            mov al,1
 
+            mov ah,09
+            mov dx,offset msg3           ;printa a mensagem
+            int 21h
+
+            mov ah,01
+            int 21h
+            mov bl,al
+
+            mov ah,09
+            mov dx,offset msg4           ;printa a mensagem
+            int 21h
+
+            mov ah,01
+            int 21h
+
+            mov cl,al
+
+            and al,01h
+            jz PAR
+            jnz IMPAR1
+
+        IMPAR1:
+            jmp IMPAR
+
+
+        PAR:
+            and bl,0fh
+            and cl,0fh
+
+            cmp cl,2
+            je LOOP2
+
+            cmp cl,4
+            je LOOP4
+
+            cmp cl,6
+            je LOOP6
+
+            cmp cl,8
+            je LOOP8a
+
+        LOOP8a:
+            jmp LOOP8
+
+
+
+        LOOP2:
+            sub cl,1
+            shl bl,cl
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+
+            xor ax,ax
+            mov al,bl
+            mov dl,10
+            div dl
+
+            mov cl,ah
+            mov bl,al
+
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+        
+
+            add bl,030h
+            mov dl,bl
+            mov ah,02
+            int 21h
+
+            add cl,030h
+            mov dl,cl
+            mov ah,02
+            int 21h
+
+            jmp FIM
+        
+        LOOP4:
+            sub cl,2
+            shl bl,cl
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+
+            xor ax,ax
+            mov al,bl
+            mov dl,10
+            div dl
+
+            mov cl,ah
+            mov bl,al
+
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+        
+
+            add bl,030h
+            mov dl,bl
+            mov ah,02
+            int 21h
+
+            add cl,030h
+            mov dl,cl
+            mov ah,02
+            int 21h
+
+            jmp FIM
+
+            LOOP6:
+            sub cl,4
+            mov al,bl
+            shl bl,cl
+            add bl,al
+            add bl,al
+
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+
+            xor ax,ax
+            mov al,bl
+            mov dl,10
+            div dl
+
+            mov cl,ah
+            mov bl,al
+
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+        
+
+            add bl,030h
+            mov dl,bl
+            mov ah,02
+            int 21h
+
+            add cl,030h
+            mov dl,cl
+            mov ah,02
+            int 21h
+
+            jmp FIM
+
+            LOOP8:
+            sub cl,5
+            shl bl,cl
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+
+            xor ax,ax
+            mov al,bl
+            mov dl,10
+            div dl
+
+            mov cl,ah
+            mov bl,al
+
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+        
+
+            add bl,030h
+            mov dl,bl
+            mov ah,02
+            int 21h
+
+            add cl,030h
+            mov dl,cl
+            mov ah,02
+            int 21h
+
+            jmp FIM
+
+        IMPAR:
+            
+            xor ax,ax
+            mov al,bl
+              
+            dec cl
+            shl bl,cl
+            add bl,al
+
+            mov ah,09
+            mov dx,offset msg5           ;printa a mensagem
+            int 21h
+
+            mov dl,bl
+            mov ah,02
+            int 21h
+
+            jmp FIM
 
         DIVICAO:
             mov al,1
